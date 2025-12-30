003cfe7df431c400174e5ddde7067bde3658b60f
Suprada-2002 <supradatripathy789@gmail.com>
2025-12-30 22:17:31 +0530

    pushing intial data

---
+++ src/lib/data/animeData.js
@@ -0,0 +1,370 @@
+export let animeData = [
+    {
+        id:1,
+        name:'Demon Slayer',
+         genre:['Action'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:2,
+        name:'Haikyu',
+         genre:['Sports', 'Vollyball'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:3,
+        name:'Naruto',
+        genre:['Action', 'Thriller'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:4,
+        name:'One Piece',
+         genre:['Action', 'Thriller'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:5,
+        name:'Fire Force',
+        genre:['Action', 'Thriller'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:6,
+        name:'Mob Phsyco 100',
+        genre:['Action'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:7,
+        name:'Violet Evergardern',
+        genre:['ROmance', 'Thriller'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:8,
+        name:'Fragnent Flower Blossoms With Dignity',
+        genre:['Romance'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:9,
+        name:'Jujitsu Kaisen',
+        genre:['Action'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:10,
+        name:'The Summer Hikaru Died',
+        genre:['Mystery', 'Horror'],
+        status: 'Completed',
+        description:'',
+        url:'',
+    },
+    {
+        id:11,
+        name:'Sk8 Infinity',
+        genre:['Skating'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:12,
+        name:'Dan Da Dan',
+        genre:['Action', 'Horror'],
+        status: 'Completed',
+        description:'',
+        url:''
+    },
+    {
+        id:13,
+        name:'Gachiakuta',
+        genre:['Action'],
+        status: 'completed',
+        description:'',
+        url:''
+    },
+    {
+        id:14,
+        name:'Death Note',
+        genre:['Action'],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:15,
+        name:'Record Of Ragnarok',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:16,
+        name:'One Punch Man',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:17,
+        name:'Attack on Titan',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:18,
+        name:'The Apothecary Diaries',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:19,
+        name:'Dororo',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:20,
+        name:'Bungo Stray Dogs',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:21,
+        name:'Howls Moving Castle',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:22,
+        name:'Tokyo Ghoul',
+        genre:[],
+        status: 'Not Watched',
+        description:'',
+        url:''
+    },
+    {
+        id:23,
+        name:'Horimiya',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:24,
+        name:'Blue Lock',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:25,
+        name:'Spy X Family',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:26,
+        name:'Bleach',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:27,
+        name:'Suzume',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:28,
+        name:'My Hero Academia',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:29,
+        name:'Frieren: Beyound Journey end',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+        {
+        id:30,
+        name:'Wind Breaker',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+        {
+        id:31,
+        name:'Sakamoto Days',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:32,
+        name:'Your Name',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:33,
+        name:'Horimiya',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:34,
+        name:'Tokoyo revengers',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:35,
+        name:'Insomnia',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:36,
+        name:'Lazarus',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:37,
+        name:'Horimiya',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:38,
+        name:'Mashle: Magic and Muscles',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:39,
+        name:'Chainsaw Man',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:40,
+        name:'Castelvania',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+        {
+        id:41,
+        name:'Hells Paradies',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:42,
+        name:'The God of Highschool',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:44,
+        name:'Yuri On Ice',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:45,
+        name:'Heavens Official Blessings',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:46,
+        name:'The Grandmaster of Demonic Cultivation',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    },
+    {
+        id:47,
+        name:'Horimiya',
+        genre:[],
+        status: 'Ongoing..',
+        description:'',
+        url:''
+    }
+]
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import {animeData} from '$lib/data/animeData'
+</script>
+
+<h1 class="header">Log of all the watched Animes</h1>
+
+<div class="anime-grid">
+    {#each animeData as data}
+        <div class="anime-card">
+            <h2>{data.name}</h2>
+            <span>{data.status}</span>
+        </div>
+    {/each}
+</div>
+
+<style>
+.anime-grid {
+  display: grid;
+  grid-template-columns: 1fr;   
+  gap: 1.25rem;      
+  max-width: 1400px; 
+  margin: 0 auto;         
+}
+
+.anime-card{
+    border: 1px solid black; 
+}
+
+.header{
+ text-align: center;
+}
+
+/* tablet */
+@media (min-width: 768px) {
+  .anime-grid {
+    grid-template-columns: repeat(3, 1fr);
+  }
+}
+
+/* desktop */
+@media (min-width: 1024px) {
+  .anime-grid {
+    grid-template-columns: repeat(5, 1fr);
+  }
+}
+
+</style>
